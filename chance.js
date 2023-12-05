c6ef59546596a935835eb568b558c3681b643480
AnneFiger <anne.figer@gmail.com>
2023-12-05 14:12:08 +0000

    Few tests passing

--- chance.js
+++ chance.js
@@ -1,5 +1,25 @@
-class Chance{
- 
+class Chance {
+
+    constructor(likelihood){
+        this.likelihood = likelihood;
+    }
+
+    getLikelihood() {
+        return this.likelihood
+    }
+
+    isImpossible() {
+        return this.likelihood === 0;
+    }
+
+    isCertain() {
+        return this.likelihood === 1;
+    }
+
+    isEqual(chance) {
+        return chance.getLikelihood() === this.likelihood;
+    }
+
 }
 
 module.exports = {Chance}
--- chance.test.js
+++ chance.test.js
@@ -2,6 +2,21 @@ const {Chance} = require("./chance")
 
 describe('Chance...', () => {
     test('the chance of a null event is impossible', () => {
-      assert(new Chance(0).isImpossible()) === true
+      expect(new Chance(0).isImpossible()).toBe(true)
+    });
+
+    test('the chance of a possible event is not impossible', () => {
+        expect(new Chance(0.5).isImpossible()).toBe(false)
+    });
+
+    test('the chance of a certain event is certain', () => {
+        expect(new Chance(1).isCertain()).toBe(true)
+    });
+    test('the chance of an impossible event is not certain', () => {
+        expect(new Chance(0).isCertain()).toBe(false)
+    });
+    test('the chance of one event is equal to the chance of another if the likelihood is the same', () => {
+        expect(new Chance(0.5).isEqual(new Chance(0.5))).toBe(true)
     })
+
 })
